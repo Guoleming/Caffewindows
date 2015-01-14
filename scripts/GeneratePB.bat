af8deacafb6e2d6f944cbc8c8f2c6978b4eae6b4
Neil Z. SHAO <initialneil@gmail.com>
2015-01-14 14:56:18 +0800

    Test prediction with ImageNet
    Use bvlc_reference_caffenet.caffemodel to do prediction in c++. Use
    caffe as .lib.

--- scripts/GeneratePB.bat
+++ scripts/GeneratePB.bat
@@ -3,6 +3,7 @@ if exist "../src/caffe/proto/caffe.pb.h" (
 ) else (
     echo caffe.pb.h is being generated
     "../3rdparty/bin/protoc" -I="../src/caffe/proto" --cpp_out="../src/caffe/proto" "../src/caffe/proto/caffe.proto"
+    copy "..\src\caffe\proto\caffe.pb.h" "..\include\caffe\caffe.pb.h"
 )
 
 if exist "../src/caffe/proto/caffe_pretty_print.pb.h" (
@@ -10,6 +11,7 @@ if exist "../src/caffe/proto/caffe_pretty_print.pb.h" (
 ) else (
     echo caffe_pretty_print.pb.h is being generated
     "../3rdparty/bin/protoc" -I="../src/caffe/proto" --cpp_out="../src/caffe/proto" "../src/caffe/proto/caffe_pretty_print.proto"
+    copy "..\src\caffe\proto\caffe_pretty_print.pb.h" "..\include\caffe\caffe_pretty_print.pb.h"
 )
 
 
